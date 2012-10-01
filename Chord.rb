fdc22e32c3e5a3d07ac74ffdbdbb718a70b879ab
doemsche <doemsche@gmx.ch>
2012-10-01 16:40:22 +0200

    regexp for chordname

--- Chord.rb
+++ Chord.rb
@@ -2,19 +2,21 @@ module MusicTheory
 
 	class Chord
 
-		attr_reader :name, :position, :root_note, :suffix, :notes
+		attr_reader :name, :position, :notes
 		
 		def initialize(options = {})
 			process_options(options)
 		end
 
 		def process_options(options)
-			@root_note = options[:root_note] unless  options[:root_note].nil?
-			@suffix = options[:suffix] unless options[:suffix].nil?
-			@name = @root_note + @suffix  unless @root_note.nil? ||  @suffix.nil?
+			@name = options[:name]
 			@position = options[:position] unless options[:position].nil?
 
-			@notes = options[:notes] unless options[:notes].nil?
+			if options[:notes].nil? and options[:name]
+				parse_notes(options[:name])
+			else
+				@notes = options[:notes] unless options[:notes].nil?
+			end
 		end
 
 		def play_chord
@@ -23,6 +25,22 @@ module MusicTheory
 			end
 		end
 
+		private
+
+		def parse_notes(name)
+			
+			regex = {
+				:root_note => /^\w{1}/,
+				:chord_name =>/^\w(maj|min|sus2|sus4)/,
+				:suffix => /(maj|min|sus2|sus4)/
+			}
+
+			root_note = name.match(regex[:root_note]).to_s
+			chord_name = name.match(regex[:chord_name]).to_s
+			suffix = name.match(regex[:suffix]).to_s
+
+			@name = chord_name
+		end
 	end
 
 end
